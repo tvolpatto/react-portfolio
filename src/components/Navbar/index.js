7786d2d505b67bc43166b5f431a7af0fbe3cf917
Thyago Volpatto <tvolpatto@gmail.com>
2020-04-18 10:25:51 -0600

    rename class to className

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -2,38 +2,38 @@ import React from 'react'
 
 export default function Navbar() {
     return (
-        <nav class="navbar is-link is-changeable is-fixed-top">
-    <div class="navbar-brand">
-      <div class="navbar-burger burger" data-target="navBarTransparent">
+        <nav className="navbar is-link is-changeable is-fixed-top">
+    <div className="navbar-brand">
+      <div className="navbar-burger burger" data-target="navBarTransparent">
         <span></span>
         <span></span>
         <span></span>
       </div>
     </div>
 
-    <div id="navBarTransparent" class="navbar-menu">
-      <div class="navbar-end">
-        <a class="navbar-item" href="#about">
-          <span class="icon">
-            <i class="fas fa-info"></i>
+    <div id="navBarTransparent" className="navbar-menu">
+      <div className="navbar-end">
+        <a className="navbar-item" href="#about">
+          <span className="icon">
+            <i className="fas fa-info"></i>
           </span>
           <span>About</span>
         </a>
-        <a class="navbar-item" href="#resume">
-          <span class="icon">
-            <i class="fas fa-file-alt"></i>
+        <a className="navbar-item" href="#resume">
+          <span className="icon">
+            <i className="fas fa-file-alt"></i>
           </span>
           <span>Resume</span>
         </a>
-        <a class="navbar-item" href="#portfolio">
-          <span class="icon">
-            <i class="fas fa-th-list"></i>
+        <a className="navbar-item" href="#portfolio">
+          <span className="icon">
+            <i className="fas fa-th-list"></i>
           </span>
           <span>Portfolio</span>
         </a>
-        <a class="navbar-item" href="#contact">
-          <span class="icon">
-            <i class="fas fa-envelope"></i>
+        <a className="navbar-item" href="#contact">
+          <span className="icon">
+            <i className="fas fa-envelope"></i>
           </span>
           <span>Contact</span>
         </a>
