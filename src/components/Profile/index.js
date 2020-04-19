eaf892223e8c8399cc5a8d2806150aeccb0f896a
Thyago Volpatto <tvolpatto@gmail.com>
2020-04-18 20:56:55 -0600

    added tbody and thead

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -3,44 +3,49 @@ import React from 'react'
 export default function Profile() {
     return (
         <div className="card-content">
-                                <h3 className="title is-4">Profile</h3>
-                                <div className="content">
-                                    <table className="table-profile">
-                                        <tr>
-                                            <th colspan="1"></th>
-                                            <th colspan="2"></th>
-                                        </tr>
-                                        <tr>
-                                            <td>Address:</td>
-                                            <td>Bluffdale - UT</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Phone:</td>
-                                            <td>(801) 792-9776</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Email:</td>
-                                            <td>tvolpatto@gmail.com</td>
-                                        </tr>
-                                    </table>
-                                </div>
-                                <br />
-                                <div className="buttons has-addons is-centered">
-                                    <a href="https://github.com/tvolpatto" target="_blank" className="button is-link is-changeable">
-                                        <span className="icon">
-                                            <i className="fab fa-github"></i>
-                                        </span>
-                                        <span>Github</span>
-                                    </a>
-                                    <a href="https://www.linkedin.com/in/thyago-volpatto-9b697539/" target="_blank" className="button is-link is-changeable">
-                                        <span className="icon">
-                                            <i className="fab fa-linkedin"></i>
-                                        </span>
-                                        <span>
-                                            LinkedIn
-                                        </span>
-                                    </a>
-                                </div>
-                            </div>
+            <h3 className="title is-4">Profile</h3>
+            <div className="content">
+                <table className="table-profile">
+                <thead>
+                    <tr>
+                        <th colSpan="1"></th>
+                        <th colSpan="2"></th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>Address:</td>
+                        <td>Bluffdale - UT</td>
+                    </tr>
+                    <tr>
+                        <td>Phone:</td>
+                        <td>(801) 792-9776</td>
+                    </tr>
+                    <tr>
+                        <td>Email:</td>
+                        <td>tvolpatto@gmail.com</td>
+                    </tr>
+
+                    </tbody>
+                </table>
+            </div>
+            <br />
+            <div className="buttons has-addons is-centered">
+                <a href="https://github.com/tvolpatto" target="_blank" className="button is-link is-changeable">
+                    <span className="icon">
+                        <i className="fab fa-github"></i>
+                    </span>
+                    <span>Github</span>
+                </a>
+                <a href="https://www.linkedin.com/in/thyago-volpatto-9b697539/" target="_blank" className="button is-link is-changeable">
+                    <span className="icon">
+                        <i className="fab fa-linkedin"></i>
+                    </span>
+                    <span>
+                        LinkedIn
+                    </span>
+                </a>
+            </div>
+        </div>
     )
 }
