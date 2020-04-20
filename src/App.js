be4cabb1ceccf8dee1d2258f0820f50ed0164510
Thyago Volpatto <tvolpatto@gmail.com>
2020-04-19 20:19:18 -0600

    fixed Contact

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import About from "./components/About";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 import Portfolio from './pages/Portfolio';
-import Contact from './pages/Contact';
+import Contact from './components/Contact';
 import Footer from './components/Footer';
 import Resume from './components/Resume';
 
@@ -19,7 +19,7 @@ function App() {
         <About/>
         <Resume/>
         <Route exact path="/portfolio" component={Portfolio} />
-        <Route exact path="/contact" component={Contact} />
+        <Contact/>
         <Footer />
       </div>
     </Router>
