3b06ec785fad693307432922efd0c1504eeeb108
Thyago Volpatto <tvolpatto@gmail.com>
2020-04-19 22:21:05 -0600

    Updates

---
+++ precache-manifest.9769037d44a7d3d1ded122b7eeb579f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa6b4039b0b0102bacb9f871f4e7294e",
+    "url": "/react-porfolio/index.html"
+  },
+  {
+    "revision": "9377d0cc9e03c94b3521",
+    "url": "/react-porfolio/static/js/2.f45e5ebf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-porfolio/static/js/2.f45e5ebf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "624d3f49766f87eb5fa3",
+    "url": "/react-porfolio/static/js/main.c3d21798.chunk.js"
+  },
+  {
+    "revision": "3825d448ad28c3265b1b",
+    "url": "/react-porfolio/static/js/runtime-main.cd2a47f0.js"
+  }
+]);
